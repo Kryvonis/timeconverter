f39b6f21e306ce5d15d8bae9d1874bfe92cc442b
kryvonis <artem.krivonos@eosda.com>
2019-06-24 14:38:39 +0300

    Init

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='timeconverter',
+    version='1.0.0',
+    packages=['timeconverter'],
+    url='https://github.com/Kryvonis/scheduledtask',
+    license='MIT',
+    author='Artem Kryvonis',
+    author_email='me@kryvonis.com',
+    description='scheduled tasks for Python 3',
+    keywords=['scheduled', 'tasks', 'cron'],
+    classifiers=[
+        'Programming Language :: Python :: 3.5',
+    ]
+)
